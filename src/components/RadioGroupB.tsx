fa9f4f55201675f33466b73210b0cbd8961f7e50
M-Farih <farih.simohammed@gmail.com>
2022-06-10 16:49:46 +0100

    fix radio button

--- src/components/RadioGroupB.tsx
+++ src/components/RadioGroupB.tsx
@@ -32,7 +32,7 @@ export const RadioGroupB: FC<RadioGroupBProps> = ({
                       relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
                 }
               >
-                {({ active, checked }) => (
+                {({ checked }) => (
                   <>
                     <div className="flex w-full items-center justify-between">
                       <div className="flex items-center">
